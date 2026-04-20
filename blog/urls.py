b0ae2a4f5427b4daef9782eaaf1ce8ffc5db4b02
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2026-04-20 15:29:24 -0400

    added template extended

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 #create a list of url patterns that when a url is visited it is tied to a function call
 
 urlpatterns = [
-    path('',views.post_list,name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
